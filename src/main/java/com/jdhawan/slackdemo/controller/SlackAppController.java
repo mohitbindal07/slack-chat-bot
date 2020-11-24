31945f5e38fdb2a047a90377572d3fd0a160f16e
mohit <mohit.bindal16@gmail.com>
2020-11-24 17:32:38 +0530

    event api 3

--- src/main/java/com/jdhawan/slackdemo/controller/SlackAppController.java
+++ src/main/java/com/jdhawan/slackdemo/controller/SlackAppController.java
@@ -2,11 +2,21 @@ package com.jdhawan.slackdemo.controller;
 
 import javax.servlet.annotation.WebServlet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.servlet.SlackAppServlet;
 
-@WebServlet("/slack/events")
+@WebServlet("/")
 public class SlackAppController extends SlackAppServlet {
+	
+	 private static final Logger logger =
+			 LoggerFactory.getLogger(SlackAppController.class);
   /**
 	 * 
 	 */
@@ -16,5 +26,12 @@ public class SlackAppController extends SlackAppServlet {
     super(app);
   }
   
-  
+	@RequestMapping(value="slack/events",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String onReceiveSlashCommand(@RequestBody String body) {
+		logger.info("Inside the event api method: "+ body);
+		return body;
+      
+    }
 }
