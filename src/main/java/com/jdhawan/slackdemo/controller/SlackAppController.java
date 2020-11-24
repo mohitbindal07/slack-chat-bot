4c351d2b427d34a659f8f06a32e3f4c6518ec090
mohit <mohit.bindal16@gmail.com>
2020-11-24 21:10:00 +0530

    event api 9

--- src/main/java/com/jdhawan/slackdemo/controller/SlackAppController.java
+++ src/main/java/com/jdhawan/slackdemo/controller/SlackAppController.java
@@ -3,13 +3,15 @@ package com.jdhawan.slackdemo.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jdhawan.slackdemo.model.SlackResponse;
+import com.hubspot.slack.client.SlackClient;
+import com.hubspot.slack.client.SlackClientFactory;
+import com.hubspot.slack.client.SlackClientRuntimeConfig;
+import com.hubspot.slack.client.methods.params.chat.ChatPostMessageParams;
 import com.slack.api.bolt.App;
 import com.slack.api.bolt.servlet.SlackAppServlet;
 
@@ -17,6 +19,7 @@ import com.slack.api.bolt.servlet.SlackAppServlet;
 @RestController("/")
 public class SlackAppController extends SlackAppServlet {
 	
+	App app = new App();
 	 private static final Logger logger =
 			 LoggerFactory.getLogger(SlackAppController.class);
   /**
@@ -28,13 +31,57 @@ public class SlackAppController extends SlackAppServlet {
     super(app);
   }
   
+	/*@RequestMapping(value="slack/events",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> onReceiveSlashCommand(@RequestBody String payloadString) {
+		logger.info("Inside the event api method: "+ payloadString);
+		
+		app.event(ReactionAddedEvent.class, (payload, ctx) -> {
+			//String payloadString = body.getBodyAsString();
+			  EventTypeExtractor eventTypeExtractor = (EventTypeExtractor) new EventsDispatcherImpl();
+			  String eventType = eventTypeExtractor.extractEventType(payloadString);
+			  if (eventType != null && eventType.equals("message")) {
+			    Gson gson = GsonFactory.createSnakeCase();
+			    MessagePayload payload2 = gson.fromJson(payloadString, MessagePayload.class);
+			    // 3. Whatever you want to do with the event data
+			  } else {
+			    // other patterns
+				  return ResponseEntity.ok("This is a String");
+			  }
+			  // 4. Respond to the Slack API server with 200 OK as an acknowledgment
+			  return ResponseEntity.ok("This is a String");
+			  ReactionAddedEvent event = body.getEvent();
+			  if (event.getReaction().equals("white_check_mark")) {
+			    ChatPostMessageResponse message = ctx.client().chatPostMessage(r -> r
+			      .channel(event.getItem().getChannel())
+			      .threadTs(event.getItem().getTs())
+			      .text("<@" + event.getUser() + "> Thank you! We greatly appreciate your efforts :two_hearts:"));
+			    if (!message.isOk()) {
+			      ctx.logger.error("chat.postMessage failed: {}", message.getError());
+			    }
+			  }
+			  return ctx.ack();
+			});
+		}
+		*/
 	@RequestMapping(value="slack/events",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> onReceiveSlashCommand(@RequestBody String body) {
-		logger.info("Inside the event api method: "+ body);
-		
-		return ResponseEntity.ok(body);
-    }
+	public void onReceiveEventCommand(@RequestBody String payload) {
+	
+	    SlackClientRuntimeConfig runtimeConfig = SlackClientRuntimeConfig.builder()
+	          .setTokenSupplier(() -> "xoxb-1507109434786-1521587020117-vkMMUmDiRYpp137YXQGqGucB").build();
+
+	        SlackClient slackClient = SlackClientFactory.defaultFactory().build(runtimeConfig);
+
+		slackClient.postMessage(
+		          ChatPostMessageParams.builder()
+		            .setText("Hey! there")
+		            .setChannelId("C01F382UQQ4")
+		            .build()
+		        ).join().unwrapOrElseThrow();
+	}
+	
 }
 
